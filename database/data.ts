d109f55f1f57e518f1acfd732cbfb30c00833528
Ana Sofia Serra <anasofiaserraluz@gmail.com>
2022-04-17 19:30:14 +0100

    Data base and api

---
+++ database/data.ts
@@ -0,0 +1,134 @@
+const data: Record<TProductId, TProduct> = {
+  '2zd33b8c': {
+    name: 'Bedspreads',
+    id: '2zd33b8c',
+    sku: 'NUR72KCM',
+    price: 50,
+    image: '/images/bedspreads.jpg',
+    attributes: {
+      description:
+        'A comfortable bedspread with a Momo-Chan and Uzagui theme',
+      shape: 'Square 200x200',
+      material: 'Cotton'
+    },
+  },
+  '6trfgkkj': {
+    name: 'Powder Box Moon',
+    id: '6trfgkkj',
+    sku: 'AX4M8SJV',
+    price: 20,
+    image: '/images/cosmetic.jpg',
+    attributes: {
+      description:
+        'Pranslucent sailor moon compact',
+      shape: 'Circular',
+      material: 'Compact Powder'
+    },
+  },
+  '7bcr49em': {
+    name: 'Artemis and Moon coffee cups',
+    id: '7bcr49em',
+    sku: 'HYA78F6J',
+    price: 40,
+    image: '/images/cups.jpg',
+    attributes: {
+      description:
+        "Set of coffee cups of the cats of the sailor squad",
+      shape: 'Circular',
+      material: 'Thermal ceramic'
+    },
+  },
+  '098323ks': {
+    name: 'Chibi Dolls Sailor Squad',
+    id: '098323ks',
+    sku: 'BXD100BLK',
+    price: 150,
+    image: '/images/dolls.jpg',
+    attributes: {
+      description:
+        'Dolls set Sailors squad Chibi style dolls',
+      shape: 'Dolls',
+      material: 'Pastic'
+    },
+  },
+  'b8uthe2y': {
+    name: 'Serena Funko Pop',
+    id: 'b8uthe2y',
+    sku: 'RMRCZN7E',
+    price: 15,
+    image: '/images/funkopop.jpg',
+    attributes: {
+      description:
+        "Funko pop Serena transformed edition",
+      shape: 'Figure',
+      material: 'Pastic'
+    },
+  },
+  'ewxsd6xb': {
+    name: 'Artemis and Moon Keychains',
+    id: 'ewxsd6xb',
+    sku: 'N55229ZA',
+    price: 10,
+    image: '/images/keychain.jpg',
+    attributes: {
+      description:
+        'Artemis and Moon Keychain Set',
+      shape: 'Kitty',
+      material: 'Aluminum and resin'
+    },
+  },
+  'fpr72m9k': {
+    name: 'Brush set',
+    id: 'fpr72m9k',
+    sku: 'B4HZ42TM',
+    price: 30,
+    image: '/images/makeup.jpg',
+    attributes: {
+      description:
+        "Set of 8 makeup brushes",
+      shape: 'Brush',
+      material: 'Synthetic'
+    },
+  },
+  't9dv25gs': {
+    name: 'NoteBook black',
+    id: 't9dv25gs',
+    sku: 'ZY3T9XXC',
+    price: 5,
+    image: '/images/notebook.jpg',
+    attributes: {
+      description:
+        'Momo-chan and usagi black notebook',
+      shape: 'Rectangular',
+      material: 'Paper'
+    },
+  },
+  't345w48y': {
+    name: 'Pens',
+    id: 't345w48y',
+    sku: 'MW79ZZ6Y',
+    price: 10,
+    image: '/images/pens.jpg',
+    attributes: {
+      description:
+        'set of 5 pens of the sailor squad',
+      shape: 'Pens',
+      material: 'Plastic'
+    },
+  },
+  't3ssdsay': {
+    name: 'Sweatshirt',
+    id: 't3ssdsay',
+    sku: 'MW79ZZ7Y',
+    price: 20,
+    image: '/images/sweatshirt.jpg',
+    attributes: {
+      description:
+        'Sweatshirt of Artemis',
+      shape: 'Sweater',
+      material: 'Cotton'
+    },
+  },
+}
+
+export default data
